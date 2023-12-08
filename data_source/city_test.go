1d36cfe278c3782695b581911333e7fbeb826914
RageNeko26 <rneko2006@gmail.com>
2023-12-08 15:00:55 +0700

    test: Change data mock

--- data_source/city_test.go
+++ data_source/city_test.go
@@ -3,14 +3,18 @@ package datasource
 import (
 	"fmt"
 	"testing"
+
+	"github.com/RNekoCloud/deep-dive-http/helper"
 )
 
 func TestModify(t *testing.T) {
 	data1 := City{
 		Name:     "Solo",
-		Province: "Sukoharjo",
+		Province: "Jawa Tengah",
 	}
 
-	Store["1"] = data1
+	id := helper.UUIDGen()
+
+	Store[id] = data1
 	fmt.Println(Store)
 }
