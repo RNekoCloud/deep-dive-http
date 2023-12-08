d2a30c464f34312c1ca15ac0e53ed7565febd774
RageNeko26 <rneko2006@gmail.com>
2023-12-08 14:15:27 +0700

    feat: Add handler for append city data

--- handler/city_handler.go
+++ handler/city_handler.go
@@ -2,7 +2,11 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+
+	datasource "github.com/RNekoCloud/deep-dive-http/data_source"
 )
 
 func Root(w http.ResponseWriter, req *http.Request) {
@@ -16,5 +20,34 @@ func Root(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusAccepted)
 	w.Write(json)
+
+}
+
+func AddCity(w http.ResponseWriter, req *http.Request) {
+	var data datasource.City
+
+	body, err := io.ReadAll(req.Body)
+
+	if err != nil {
+		fmt.Println("Failed to parse payload request:", err)
+		return
+	}
+
+	err = json.Unmarshal(body, &data)
+
+	if err != nil {
+		fmt.Println("Failed to parsing JSON:", err)
+		return
+	}
+
+	res := map[string]interface{}{
+		"message": "Successfully add new data",
+		"data":    body,
+	}
+
+	b, _ := json.Marshal(res)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
 
 }
